8f1d4effcb2aa02eb0a644f11420b1baf749d562
gefgu <gefgu@hotmail.com>
2022-08-23 16:42:52 -0300

    Add merge

--- mergeSort.js
+++ mergeSort.js
@@ -3,15 +3,38 @@
 function mergeSort(array) {
   const size = array.length;
   if (size === 1) return array;
-  const left = mergeSort(array.slice(0, Math.floor(array.length / 2)));
-  const rigth = mergeSort(array.slice(Math.floor(array.length / 2)));
+  let left = mergeSort(array.slice(0, Math.floor(array.length / 2)));
+  let right = mergeSort(array.slice(Math.floor(array.length / 2)));
 
+  let newArray = [];
+  let firstLeft = left.shift();
+  let firstRight = right.shift();
+  while (newArray.length < size) {
+    if (firstLeft === undefined) {
+      newArray = newArray.concat([firstRight, ...right]);
+      continue;
+    }
 
-  return array;
+    if (firstRight === undefined) {
+      newArray = newArray.concat([firstLeft, ...left]);
+      continue
+    }
+
+    if (firstLeft <= firstRight) {
+      newArray.push(firstLeft);
+      firstLeft = left.shift();
+    } else {
+      newArray.push(firstRight);
+      firstRight = right.shift();
+    }
+  }
+
+  return newArray;
 }
 
 console.log(mergeSort([1, 2, 3]));
-// console.log(mergeSort([3, 2, 1]));
-// console.log(mergeSort([3, 2, 1]));
-// console.log(mergeSort([1, 3, 5, 2, 4]));
-// console.log(mergeSort([5, 4, 3, 2, 1]));
+console.log(mergeSort([3, 2, 1]));
+console.log(mergeSort([3, 2, 1]));
+console.log(mergeSort([1, 3, 5, 2, 4]));
+console.log(mergeSort([5, 4, 3, 2, 1]));
+console.log(mergeSort([1, 1, 3, 1, 1]));
