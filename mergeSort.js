2f39c10e0be0b986ee89e9298f7ee8f144c1a0ad
gefgu <gefgu@hotmail.com>
2022-08-23 16:24:24 -0300

    Add simple tests to merge sort

---
+++ mergeSort.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function mergeSort(array) {
+  return array;
+}
+
+console.log(mergeSort([1, 2, 3]));
+console.log(mergeSort([3, 2, 1]));
+console.log(mergeSort([3, 2, 1]));
+console.log(mergeSort([1, 3, 5, 2, 4]));
+console.log(mergeSort([5, 4, 3, 2, 1]));
